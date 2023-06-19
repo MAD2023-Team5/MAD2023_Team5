b6732577b7f04fcdf96981ee9a0722cbe2e682e5
LudovicTan <S10223654@connect.np.edu.sg>
2023-06-19 23:27:35 +0800

    Added Browsing Page?

---
+++ app/src/main/java/com/example/np/mad/happy_habbits/BrowsingRoutines.java
@@ -0,0 +1,75 @@
+package com.example.np.mad.happy_habbits;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.AdapterView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// WorkoutRoutinesActivity.java
+public class BrowsingRoutines extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private WorkoutRoutinesAdapter adapter;
+    private DatabaseReference firebaseData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browsing_routine);
+
+        recyclerView = findViewById(R.id.BrowsingRoutinesRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize Firebase
+        firebaseData = FirebaseDatabase.getInstance().getReference("Routines");
+
+        // Set up adapter
+        adapter = new WorkoutRoutinesAdapter(new ArrayList<>(), new WorkoutRoutinesAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Routine workoutRoutine) {
+                // Handle item click
+                // You can pass the selected workoutRoutine object to another activity or fragment for further processing
+                // For example:
+                // Intent intent = new Intent(WorkoutRoutinesActivity.this, WorkoutDetailActivity.class);
+                // intent.putExtra("workoutRoutine", workoutRoutine);
+                // startActivity(intent);
+            }
+        });
+        recyclerView.setAdapter(adapter);
+
+        // Retrieve workout routines data from Firebase
+        retrieveWorkoutRoutines();
+    }
+
+    private void retrieveWorkoutRoutines() {
+        firebaseData.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Routine> workoutRoutines = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Routine workoutRoutine = snapshot.getValue(Routine.class);
+                    workoutRoutines.add(workoutRoutine);
+                }
+                adapter.workoutRoutines = workoutRoutines;
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle database error
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/np/mad/happy_habbits/WorkoutRoutinesAdapter.java
@@ -0,0 +1,73 @@
+package com.example.np.mad.happy_habbits;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+// WorkoutRoutinesAdapter.java
+public class WorkoutRoutinesAdapter extends RecyclerView.Adapter<WorkoutRoutinesAdapter.ViewHolder>{
+    List<Routine> workoutRoutines;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        void onItemClick(Routine workoutRoutine);
+    }
+
+    public WorkoutRoutinesAdapter(List<Routine> workoutRoutines, OnItemClickListener listener) {
+        this.workoutRoutines = workoutRoutines;
+        this.listener = listener;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewWorkoutRoutineName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textViewWorkoutRoutineName = itemView.findViewById(R.id.textViewSelectedRoutineName);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View workoutRoutineView = inflater.inflate(R.layout.selected_routine, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(workoutRoutineView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Get the data model based on position
+        Routine workoutRoutine = workoutRoutines.get(position);
+
+        // Set item views based on your views and data model
+        TextView textViewWorkoutRoutineName = holder.textViewWorkoutRoutineName;
+        textViewWorkoutRoutineName.setText(workoutRoutine.getRoutineNo());
+
+        // Set click listener
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onItemClick(workoutRoutine);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return workoutRoutines.size();
+    }
+}
