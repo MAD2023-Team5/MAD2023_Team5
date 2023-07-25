9fc71c9acabcc412b368ed5aa3b22d2744e2abd6
Clairerang <s10223223@connect.np.edu.sg>
2023-07-25 12:51:19 +0800

    Base files for Calorie Tracker (no codes in)

---
+++ app/src/main/java/sg/edu/np/mad/happyhabit/ui/CalorieTracker/CalorieTrackerAdapter.java
@@ -0,0 +1,4 @@
+package sg.edu.np.mad.happyhabit.ui.CalorieTracker;
+
+public class CalorieTrackerAdapter {
+}
---
+++ app/src/main/java/sg/edu/np/mad/happyhabit/ui/CalorieTracker/CalorieTrackerViewModel.java
@@ -0,0 +1,7 @@
+package sg.edu.np.mad.happyhabit.ui.CalorieTracker;
+
+import androidx.lifecycle.ViewModel;
+
+public class CalorieTrackerViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
